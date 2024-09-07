31124bb47f7e82ef6cbb3f8ca8a0e27067a16bf4
edwardsila <edwardsila000@gmail.com>
2024-09-07 19:41:49 +0300

    my fisrt template

--- inkstore/store/views.py
+++ inkstore/store/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+'''Create your views here.'''
+
+def home(request):
+    ''' view for our home page '''
+    return render(request, 'home.html', {})
