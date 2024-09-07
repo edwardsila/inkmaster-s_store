c5fd479c0649bd33b6e5ca57878e73a4b61690f3
edwardsila <edwardsila000@gmail.com>
2024-09-07 20:18:47 +0300

    register models

--- inkstore/store/admin.py
+++ inkstore/store/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Orders)
