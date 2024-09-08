bdf024cd3fc87fca98b631373cfe422304e850ec
edwardsila <edwardsila000@gmail.com>
2024-09-08 12:10:28 +0300

    logic on user authentication login logout page

--- inkstore/store/views.py
+++ inkstore/store/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 
 '''Create your views here.'''
 
@@ -12,3 +15,10 @@ def home(request):
 def about(request):
     ''' view for our home page '''
     return render(request, 'about.html', {})
+    
+def login_user(request):
+    ''' view for user login '''
+    return render(request, 'login.html', {})
+    
+def logout_user(request):
+    pass
