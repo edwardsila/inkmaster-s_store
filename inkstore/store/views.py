d0fcc9e6b8bb131807ffbb69205f4e900e19b97c
edwardsila <edwardsila000@gmail.com>
2024-09-08 12:51:49 +0300

    user authentication on login form

--- inkstore/store/views.py
+++ inkstore/store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -18,7 +18,24 @@ def about(request):
     
 def login_user(request):
     ''' view for user login '''
-    return render(request, 'login.html', {})
+    ''' did they fill out the login form?? '''
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have been logged in"))
+            return redirect('home')
+        else:
+            messages.success(request, ("There was an error, try again later...."))
+            return redirect('login')
+    
+    else:    
+        return render(request, 'login.html', {})
     
 def logout_user(request):
-    pass
+    ''' view for user logout '''
+    logout(request)
+    messages.success(request, ("You have been logged out!"))
+    return redirect('home')
