f80c95ae48fd10ca776d8500cc1cf3980899930e
edwardsila <edwardsila000@gmail.com>
2024-09-08 11:39:11 +0300

    about page

--- inkstore/store/views.py
+++ inkstore/store/views.py
@@ -8,3 +8,7 @@ def home(request):
     ''' view for our home page '''
     products = Product.objects.all()
     return render(request, 'home.html', {'products': products})
+    
+def about(request):
+    ''' view for our home page '''
+    return render(request, 'about.html', {})
