815e405886d3fc88e0f0adab9467cda9392ba41a
edwardsila <edwardsila000@gmail.com>
2024-09-11 23:00:16 +0300

    extend user profile

--- inkstore/store/admin.py
+++ inkstore/store/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Category, Customer, Product, Orders
+from .models import Category, Customer, Product, Orders, Profile
+from django.contrib.auth.models import User
 
 # Register your models here.
 
@@ -7,3 +8,21 @@ admin.site.register(Category)
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Orders)
+admin.site.register(Profile)
+
+
+''' mix profile info and user info '''
+class ProfileInline(admin.StackedInline):
+	model = Profile
+
+''' extend user model '''
+class UserAdmin(admin.ModelAdmin):
+	model = User
+	field = ["username", "first_name", "last_name", "email"]
+	inlines = [ProfileInline]
+
+''' unregiste the old way '''
+admin.site.unregister(User)
+
+''' re register the new way '''
+admin.site.register(User, UserAdmin)
--- inkstore/store/models.py
+++ inkstore/store/models.py
@@ -1,7 +1,36 @@
 from django.db import models
 from datetime import datetime
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
+
+''' profile model '''
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date_modified = models.DateTimeField(User, auto_now=True)
+    phone = models.CharField(max_length=20, blank=True)
+    address1 = models.CharField(max_length=200, blank=True)
+    address2 = models.CharField(max_length=200, blank=True)
+    city = models.CharField(max_length=200, blank=True)
+    state = models.CharField(max_length=200, blank=True)
+    zipcode = models.CharField(max_length=200, blank=True)
+    Country = models.CharField(max_length=200, blank=True)
+
+    def __Str__(self):
+        return self.user.username
+
+''' create user profile when user signs up by default '''
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+''' automate profile thing '''
+post_save.connect(create_profile, sender=User)
+
+
+
 
 ''' Category model '''
 
