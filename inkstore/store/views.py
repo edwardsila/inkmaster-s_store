1592f2ce0f5d33ae9d0d7dd697d27d699ef9acdf
edwardsila <edwardsila000@gmail.com>
2024-09-12 20:43:37 +0300

    update shipping info

--- inkstore/store/views.py
+++ inkstore/store/views.py
@@ -6,6 +6,10 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from .forms import SignUpForm, UpdateUserForm, ChangePasswordForm, UserInfoForm
+
+from payment.forms import ShippingForm
+from payment.models import ShippingAddress
+
 from django.db.models import Q
 import json
 from cart.cart import Cart
@@ -34,16 +38,25 @@ def search(request):
 def update_info(request):
     if request.user.is_authenticated:
         ''' need to know which user '''
+        ''' get current user '''
         current_user = Profile.objects.get(user__id=request.user.id)
+        ''' get current user's shipping info '''
+        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+
+        ''' get original User form '''
         form = UserInfoForm(request.POST or None, instance=current_user)
+        ''' get User's shipping form '''
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
 
-        if form.is_valid():
+        if form.is_valid() or shipping_form.is_valid():
+            ''' save original form '''
             form.save()
-
+            ''' save shipping form '''
+            shipping_form.save()
             messages.success(request, "Your info has been updated!!")
             return redirect('home')
 
-        return render(request, "update_info.html", {'form':form})
+        return render(request, "update_info.html", {'form':form, 'shipping_form':shipping_form})
 
     else:
         messages.success(request, "You must be logged in to access that page!!")
