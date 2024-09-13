720936da0a3f8ae8af920593f6c70b2d06f865bf
edwardsila <edwardsila000@gmail.com>
2024-09-13 16:27:29 +0300

    billing info

--- inkstore/payment/views.py
+++ inkstore/payment/views.py
@@ -1,9 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cart.cart import Cart
-from payment.forms import ShippingForm
+from payment.forms import ShippingForm, PaymentForm
 from payment.models import ShippingAddress
+from django.contrib import messages
 
 # Create your views here.
+
+def billing_info(request):
+	if request.POST:
+		''' get tghe cart '''
+		cart = Cart(request)
+		cart_products = cart.get_product
+
+		quantities = cart.get_quantity
+		totals = cart.cart_total()
+
+		''' check to see if user is logged in '''
+		if request.user.is_authenticated:
+			''' get the billing form '''
+			billing_form = PaymentForm()
+			return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_info":request.POST, "billing_form":billing_form})
+		else:
+			''' not logged in '''
+			''' get the billing form '''
+			billing_form = PaymentForm()
+			return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_info":request.POST, "billing_form":billing_form})
+
+		shipping_form = request.POST
+		return render(request, "payment/billing_info.html", {"cart_products": cart_products, "quantities": quantities, "totals": totals, "shipping_form":shipping_form})
+	else:
+		messages.success(request, "Access denied")
+		return redirect('home')
 
 def checkout(request):
 
