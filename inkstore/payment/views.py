10c2d0e21888330bfac770fbe6ec6ea84d2dfcac
edwardsila <edwardsila000@gmail.com>
2024-09-14 09:40:54 +0300

    admin dhasboard item page

--- inkstore/payment/views.py
+++ inkstore/payment/views.py
@@ -9,6 +9,19 @@ from store.models import Product
 
 # Create your views here.
 
+def orders(request, pk):
+	if request.user.is_authenticated and request.user.is_superuser:
+		''' get the order '''
+		order = Order.objects.get(id=pk)
+		''' get order items'''
+		items = OrderItem.objects.filter(order=pk)
+
+		return render(request, "payment/orders.html", {"order":order, "items":items})
+	else:
+		messages.success(request, "Access denied")
+		return redirect('home')
+
+
 
 def not_shipped_dash(request):
 	if request.user.is_authenticated and request.user.is_superuser:
