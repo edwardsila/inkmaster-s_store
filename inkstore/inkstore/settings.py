db7d6ec3b24a3a9730ef21d3bf8ed1bb155dbc05
edwardsila <edwardsila000@gmail.com>
2024-09-16 22:06:03 +0300

    add postgres cloud database

--- inkstore/inkstore/settings.py
+++ inkstore/inkstore/settings.py
@@ -12,9 +12,16 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+''' load our enviromental variables '''
+load_dotenv()
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -78,10 +85,17 @@ WSGI_APPLICATION = 'inkstore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': 'junction.proxy.rlwy.net',
+        'PORT': '40327',
     }
 }
 
